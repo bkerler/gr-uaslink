77742e0e7108464c52948552e2daf9c1a0d74211
Eric Schneider <eric@closedloopdesign.com>
2017-11-17 11:39:40 -0800

    checkpoint before overwrite with hackfest pc wip.

--- apps/zmq_serial_mavlink.py
+++ apps/zmq_serial_mavlink.py
@@ -19,8 +19,8 @@ def string_to_hex(data):
 
 class ZMQ_Serial_Mavlink():
     def __init__(self,port="/dev/ttyACM0",baud=57600,
-            zmq_sub_addr="tcp://127.0.0.1:14000",
-            zmq_pub_addr="tcp://127.0.0.1:14001"):
+            zmq_sub_addr="tcp://127.0.0.1:14001",
+            zmq_pub_addr="tcp://127.0.0.1:14000"):
         
         self.ser_port=port
         self.ser_baud=baud
@@ -28,6 +28,8 @@ class ZMQ_Serial_Mavlink():
         self.quit=False
         
         self.ctx = zmq.Context()
+        
+        self.zmq_meta=dict()
         
         #zmq tx
         self.tx_sock = self.ctx.socket(zmq.PUB)
@@ -48,6 +50,9 @@ class ZMQ_Serial_Mavlink():
         self._t_ser = threading.Thread(target=self.serial_rx_thread_proc)
         self._t_ser.start()
         self._t_zmq = threading.Thread(target=self.zmq_rx_thread_proc)
+        self._t_zmq.start()
+
+        self._t_meta_relay = threading.Thread(target=self.meta_relay_thread_proc)
         self._t_zmq.start()
         
     def stop(self):
@@ -70,7 +75,7 @@ class ZMQ_Serial_Mavlink():
         print "exit:serial_rx_thread_proc"
             
     def zmq_rx_thread_proc(self):
-        print "enter:zmq_rx_thread_proc"
+        print "enter:zmq_rx_thread_roc"
         
         poller = zmq.Poller()
         poller.register(self.rx_sock, zmq.POLLIN)
@@ -81,22 +86,57 @@ class ZMQ_Serial_Mavlink():
                 msg = self.rx_sock.recv()
             
                 pdu = pmt.deserialize_str(msg)
-                car = pmt.to_python(pmt.car(pdu))
-                cdr = pmt.to_python(pmt.cdr(pdu))
-                cdr = numpy.getbuffer(cdr)  # flatten the vector bytes
+                meta = pmt.to_python(pmt.car(pdu))
+                data = pmt.to_python(pmt.cdr(pdu))
+                data = numpy.getbuffer(data)  # flatten the vector bytes
                 
                 if car is not None:
-                    print "zmq_rx:",car  # Print the dict
+                    print "zmq_rx:",meta  # Print the dict
                     # print '[car] Received [%i] bytes: %s' % (len(car), string_to_hex(car))
-                if cdr is not None:
-                    print 'zmq_rx:[cdr] Received [%i] bytes: %s' % (len(cdr), string_to_hex(cdr))
+                    self.zmq_meta.update(meta)
+                if data is not None:
+                    #print 'zmq_rx:[cdr] Received [%i] bytes: %s' % (len(cdr), string_to_hex(cdr))r
+                    print 'zmq_rx:[cdr] Received [%i] bytes' % (len(data))
 
-                #self.ser.write(cdr.tobytes())
-                self.ser.write(cdr)
+                #self.ser.write(data.tobytes())
+                self.ser.write(data)
 
         print "exit:zmq_rx_thread_proc"
 
+    def meta_relay_thread_proc(self):
+        """send some radio link meta data on the reverse link
+        
+        {'cfo': 100.9404296875, 'npkt_ok': 4, 'header_pass_rate': 100.0, 
+        'es::event_length': 8192L, 'timestamp': 1510259309.3105321, 'est_len': 8299, 
+        'orig_len': 8192, 'rx_rate': (0L, 199999.99937668347), 'es::event_type': 'edge_event',
+        'es::event_time': 367994L, 'sync_delay': 8300, 'npkt_hok': 4, 
+        'es::event_buffer': (array([  0,   0,   0, ...,   1,  16, 187], dtype=uint8), None),
+        'payload_pass_rate': 100.0, 'rx_freq': (0L, 3017000000.0), 
+        'rx_time': (0L, (1L, 0.60202746593035106)), 'npkt': 4}
+        """
+        while not self.quit:
+            self.zmq_meta=dict()
+            d=self.zmq_meta
+            #bits = Bits(hex="13371337")
+            #bits = Bits(int=int(d.get('header_pass_rate',0)), length=8)
+            
+            s="hpr:%.1f,ppr:%.1f,cfo:%.2f"%( d.get('header_pass_rate',0.0), 
+                d.get('payload_pass_rate',0.0),d.get('cfo',0.0))
+                
+            car = pmt.make_dict()
+            cdr = pmt.to_pmt(data)
+            pdu = pmt.cons(car, cdr)
 
+            # Status debug
+            # print meta, type(meta), '\tmeta'
+            # print pmtdata, type(pmtdata), '\tpmtdata'
+            # print msg, type(msg), '\tmsg'
+
+            #print 'send_zmq: Sending [%d] byte payload: %s' % (len(data), string_to_hex(data))
+            self.tx_sock.send(pmt.serialize_str(pdu))
+
+            time.sleep(1)
+            
     def send_zmq(self, data):
         # Build the PDU
         car = pmt.make_dict()
@@ -108,10 +148,10 @@ class ZMQ_Serial_Mavlink():
         # print pmtdata, type(pmtdata), '\tpmtdata'
         # print msg, type(msg), '\tmsg'
 
-        print 'send_zmq: Sending [%d] byte payload: %s' % (len(data), string_to_hex(data))
+        #print 'send_zmq: Sending [%d] byte payload: %s' % (len(data), string_to_hex(data))
         self.tx_sock.send(pmt.serialize_str(pdu))
 
-        
+    
         
 if __name__ == "__main__":
     zs=ZMQ_Serial_Mavlink()
